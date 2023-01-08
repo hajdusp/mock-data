4d92fdc430b44e543c43ad1f172fdf22b42f308a
hajdusp <szegedweb@gmail.com>
2023-01-08 19:12:27 +0100

    terminators added

---
+++ terminators/models.mock.js
@@ -0,0 +1,47 @@
+export const modelsMock= [
+    {
+        id: 1,
+        name: "T-100",
+        serialNumber: 1001
+    },
+    {
+        id: 2,
+        name: "T-200",
+        serialNumber: 1002
+    },
+    {
+        id: 3,
+        name: "T-300",
+        serialNumber: 1003
+    },
+    {
+        id: 4,
+        name: "T-400",
+        serialNumber: 1004
+    },
+    {
+        id: 5,
+        name: "T-500",
+        serialNumber: 1005
+    },
+    {
+        id: 6,
+        name: "T-600",
+        serialNumber: 1006
+    },
+    {
+        id: 7,
+        name: "T-700",
+        serialNumber: 1007
+    },
+    {
+        id: 8,
+        name: "T-800",
+        serialNumber: 1008
+    },
+    {
+        id: 9,
+        name: "T-900",
+        serialNumber: 1009
+    }
+];
